46ae96cc002d520f8fc100212ce60ede8dcae614
Ryusei Tomari <ryusei@RyuseinoMacBook-Air.local>
2025-11-02 15:20:44 +0900

    戻るボタン/進むボタン実装

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // サイドバーのアクティブ状態の制御
   const buttons = document.querySelectorAll(".sidebar .sidebar-btn");
 
   const normalize = (p) => {
@@ -25,5 +26,38 @@ document.addEventListener("DOMContentLoaded", () => {
         btn.classList.add("active");
       }
     } catch (err) {}
+  });
+
+  // 戻る / 進むボタンの実装
+  const backBtn = document.querySelector(".navbar-back-btn");
+  const forwardBtn = document.querySelector(".navbar-forward-btn");
+  const backImg = backBtn?.querySelector("img");
+  const forwardImg = forwardBtn?.querySelector("img");
+
+  const updateNavButtons = () => {
+    // 戻る履歴の有無で backBtn の状態を更新
+    if (window.history.length > 1) {
+      backBtn?.removeAttribute("disabled");
+      backImg.src = "./images/back-active.png";
+
+      forwardBtn?.removeAttribute("disabled");
+      forwardImg.src = "./images/forward-active.png";
+    } else {
+      backBtn?.setAttribute("disabled", "true");
+      backImg.src = "./images/back-inactive.png";
+
+      forwardBtn?.setAttribute("disabled", "true");
+      forwardImg.src = "./images/forward-inactive.png";
+    }
+  };
+
+  updateNavButtons();
+
+  backBtn?.addEventListener("click", () => {
+    history.back();
+  });
+
+  forwardBtn?.addEventListener("click", () => {
+    history.forward();
   });
 });
