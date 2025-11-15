8dc7b7e73f62d23a38d6e3c3a2c68cd5e5611b75
RyuseiTomari <r.tomari@roots.gr.jp>
2025-11-15 17:30:11 +0900

    jsファイル分割

--- js/index.js
+++ js/index.js
@@ -146,20 +146,4 @@ document.addEventListener("DOMContentLoaded", () => {
       }, 150);
     }
   });
-
-  // タブメニューの切り替え
-  const tabButtons = document.querySelectorAll(".tab-button");
-  const tabPanels = document.querySelectorAll(".tab-panel");
-
-  tabButtons?.forEach((button) => {
-    button.addEventListener("click", () => {
-      const targetTab = button.dataset.tab;
-
-      tabButtons?.forEach((btn) => btn.classList.remove("active"));
-      tabPanels?.forEach((panel) => panel.classList.remove("active"));
-
-      button.classList.add("active");
-      document.querySelector(`[data-panel="${targetTab}"]`).classList.add("active");
-    });
-  });
 });
